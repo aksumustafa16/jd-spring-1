ac304080db44666d86acd7135f7bbb8ad5156bf7
musaksu <musaksu@gmail.com>
2022-12-15 09:45:00 +0100

    JavaAnnotations-Autowired

--- Spring-05-JavaAnnotations-Autowired/src/main/java/com/cybertek/services/Java.java
+++ Spring-05-JavaAnnotations-Autowired/src/main/java/com/cybertek/services/Java.java
@@ -1,12 +1,18 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Java implements Course {
 
     private OfficeHours officeHours;
+
+    @Autowired
+    public Java(OfficeHours officeHours) {
+        this.officeHours = officeHours;
+    }
 
     @Override
     public void getTeachingHours() {
--- Spring-05-JavaAnnotations-Autowired/src/main/java/com/cybertek/services/OfficeHours.java
+++ Spring-05-JavaAnnotations-Autowired/src/main/java/com/cybertek/services/OfficeHours.java
@@ -1,7 +1,9 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.ExtraSeesions;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OfficeHours implements ExtraSeesions {
     @Override
     public int getHours() {
